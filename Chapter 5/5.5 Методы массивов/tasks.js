e23c24f259e8cff53901638bb0a3715f1da7fda2
Senin Maksim <wecpo@yandex.ru>
2024-03-26 21:08:27 +0300

    Chapter I, 5.5 myCode done, tasks - only first done

---
+++ Chapter 5/5.5 Методы массивов/tasks.js
@@ -0,0 +1,27 @@
+// Переведите текст вида border-left-width в borderLeftWidth
+// важность: 5
+// Напишите функцию camelize(str), которая преобразует строки вида «my-short-string» в «myShortString».
+
+// То есть дефисы удаляются, а все слова после них получают заглавную букву.
+
+// Примеры:
+
+// camelize("background-color") == 'backgroundColor';
+// camelize("list-style-image") == 'listStyleImage';
+// camelize("-webkit-transition") == 'WebkitTransition';
+// P.S. Подсказка: используйте split, чтобы разбить строку на массив символов,
+//  потом переделайте всё как нужно и методом join соедините обратно.
+
+() => {
+  function camelize(str) {
+    let res = str.split("-");
+    return res
+      .map((word, index) => {
+        return index === 0 ? word : word[0].toUpperCase() + word.slice(1);
+      })
+      .join("");
+  }
+  camelize("background-color");
+  camelize("list-style-image");
+  camelize("-webkit-transition");
+};
