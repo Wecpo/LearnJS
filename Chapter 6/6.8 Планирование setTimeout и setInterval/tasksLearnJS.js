0d12be89f012e1ac28e5bfe8ae112d27a7979546
Senin Maksim <wecpo@yandex.ru>
2024-06-21 08:03:49 +0300

    6.6, 6.7, 6.8 done

---
+++ Chapter 6/6.8 Планирование setTimeout и setInterval/tasksLearnJS.js
@@ -0,0 +1,42 @@
+// Вывод каждую секунду
+// важность: 5
+// Напишите функцию printNumbers(from, to), которая выводит число каждую секунду,
+//  начиная от from и заканчивая to.
+
+// Сделайте два варианта решения.
+
+// Используя setInterval.
+// Используя рекурсивный setTimeout.
+
+() => {
+  // setInterval
+  function printNumbers(from, to) {
+    let current = from;
+    const intervalId = setInterval(() => {
+      console.log(current);
+      if (current === to) {
+        clearInterval(intervalId);
+      }
+      current++;
+    }, 1000);
+  }
+
+  printNumbers(1, 5);
+};
+
+() => {
+  // setTimeout
+  function printNumbers(from, to) {
+    let current = from;
+    setTimeout(function log() {
+      console.log(current);
+      if (current < to) {
+        current++;
+        setTimeout(log, 1000);
+      }
+      clearTimeout(setTimeoutId);
+    }, 1000);
+  }
+
+  printNumbers(1, 5);
+};
