914f80909080c6f2eb6f94f317c76ef3e4c9dd5e
Senin Maksim <wecpo@yandex.ru>
2024-09-12 15:28:12 +0300

    feat: 11.4 done.

--- LJS/Chapter 11/11.4 Промисы - обработка ошибок/tasks.js
+++ LJS/Chapter 11/11.4 Промисы - обработка ошибок/tasks.js
@@ -1,1 +1,10 @@
- 
+// Ошибка в setTimeout
+// Что вы думаете? Выполнится ли .catch? Поясните свой ответ.
+
+new Promise(function (resolve, reject) {
+  setTimeout(() => {
+    throw new Error("Whoops!");
+  }, 1000);
+}).catch(alert);
+
+// catch не выполнится, тк ошибка не синхронная, скрипт упадет
