2c47485318c581029d3d90654c13193572c56acd
Senin Maksim <wecpo@yandex.ru>
2024-09-14 16:17:47 +0300

    feat: 11.4, 11.5 done.

--- LJS/Chapter 11/11.4 Промисы - обработка ошибок/summary.js
+++ LJS/Chapter 11/11.4 Промисы - обработка ошибок/summary.js
@@ -232,10 +232,10 @@
 //   благодаря чему наше приложение никогда не будет «просто умирать».
 
 window.addEventListener("unhandledrejection", function (event) {
-    // объект события имеет два специальных свойства:
-    alert(event.promise); // [object Promise] - промис, который сгенерировал ошибку
-    alert(event.reason); // Error: Ошибка! - объект ошибки, которая не была обработана
-  });
+  // объект события имеет два специальных свойства:
+  alert(event.promise); // [object Promise] - промис, который сгенерировал ошибку
+  alert(event.reason); // Error: Ошибка! - объект ошибки, которая не была обработана
+});
 
 const pr = new Promise((res, rej) => {
   res(1);
